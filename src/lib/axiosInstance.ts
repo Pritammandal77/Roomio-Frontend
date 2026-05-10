72e81a21486d9ffa850de7815d9cec16778db114
Pritam Mandal <155251107+Pritammandal77@users.noreply.github.com>
2026-05-10 15:14:56 +0530

    fix: removed the middleware and proxy

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,63 +1,26 @@
-// import axios from "axios";
-
-
-// export const axiosInstance = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-//   withCredentials: true,
-// });
-
-// axiosInstance.interceptors.response.use(
-//     (res) => res,
-//     async (error) => {
-//         const originalRequest = error.config;
-
-//         if (
-//       error.response?.status === 401 &&
-//       !originalRequest._retry &&
-//       !originalRequest.url.includes("/refresh-access-token")
-//     ) {
-//         originalRequest._retry = true;
-
-//       try {
-//           await axiosInstance.post("/api/user/refresh-access-token");
-//           return axiosInstance(originalRequest);
-//         } catch (err) {
-//         console.log("Refresh failed → logout user");
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-
-
-// with proxy
 import axios from "axios";
-const isProduction = process.env.NODE_ENV === "production";
 
 export const axiosInstance = axios.create({
-  baseURL: isProduction
-    ? "/"                                        // prod → go through Next.js proxy
-    : process.env.NEXT_PUBLIC_API_BASE_URL,      // local → hit Render/localhost directly
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
 
 axiosInstance.interceptors.response.use(
-  (res) => res,
-  async (error) => {
-    const originalRequest = error.config;
+    (res) => res,
+    async (error) => {
+        const originalRequest = error.config;
 
-    if (
+        if (
       error.response?.status === 401 &&
       !originalRequest._retry &&
       !originalRequest.url.includes("/refresh-access-token")
     ) {
-      originalRequest._retry = true;
+        originalRequest._retry = true;
 
       try {
-        await axiosInstance.post("/api/user/refresh-access-token");
-        return axiosInstance(originalRequest);
-      } catch (err) {
+          await axiosInstance.post("/api/user/refresh-access-token");
+          return axiosInstance(originalRequest);
+        } catch (err) {
         console.log("Refresh failed → logout user");
       }
     }
